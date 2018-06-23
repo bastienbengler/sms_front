03d0695931fe0ee92bdaa670bf275311dfd8bd1b
Bastien Bengler <bastien.bengler@epitech.eu>
2018-06-23 17:14:54 +0200

    phonenumber component 1.0 beta

--- src/components/PhoneNumber/PhoneNumber.js
+++ src/components/PhoneNumber/PhoneNumber.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import './PhoneNumber.css';
 import Iphone from 'react-icons/lib/io/iphone';
-/*import { Observable, from, fromEvent } from 'rxjs';
-import { map, filter, switchMap } from 'rxjs/operators';
-import $ from 'jquery'; 
-*/
+import { fromEvent } from 'rxjs';
+import $ from 'jquery';
+
 export default class PhoneNumber extends React.Component {
 	static propTypes = {
 	};
@@ -11,31 +11,39 @@ export default class PhoneNumber extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			phone: ""
+			phone: "",
+			showValidation: false
 		};
 	}
 	componentDidMount() {
-		/*fromEvent($( "#form_phone_number" ), 'keydown')
-		.subscribe((e) => console.log(e));*/
+		fromEvent($( "#form_phone_number" ), 'click')
+		.subscribe((e) => {
+			this.setState({
+				showValidation: true
+			});
+		});
+	}
+
+	handleForm() {
+		alert("ici");
 	}
 
 	render() {
 		return (
 			<div>
-				<form className="form-inline was-validated" id="form_phone_number" onSubmit={(e) => {e.preventDefault(); console.log("stop");}} noValidate>
-				<label className="sr-only" htmlFor="inlineFormInputGroupUsername2">Username</label>
-					<div className="input-group mb-2 mr-sm-2">
+				<form className={'needs-validation row justify-content-center no-gutters form-inline mb-1 ' + (this.state.showValidation ? 'was-validated' : '' )} id="form_phone_number" onSubmit={(e) => {e.preventDefault(); this.handleForm();}} noValidate>
+					<div className="col-sm-4 col-12 row no-gutters justify-content-center input-group" style={{display: 'inherit'}}>
 						<div className="input-group-prepend">
-							<div className="input-group-text" style={{fontSize: '2em'}}><Iphone /></div>
+							<div className="input-group-text size-icon"><Iphone /></div>
 						</div>
-						<input type="text" className="form-control" id="inlineFormInputGroupUsername2" placeholder="+33 ..."
+						<input type="text" className="col-sm-12 form-control" id="inlineFormInputGroupUsername2" placeholder="+33 ..."
 						pattern="^(?:(?:\+|00)33[\s.-]{0,3}(?:\(0\)[\s.-]{0,3})?|0)[1-9](?:(?:[\s.-]?\d{2}){4}|\d{2}(?:[\s.-]?\d{3}){2})$" required/>
 						<div className="invalid-feedback">
 				        	Veuillez renseigner un numéro de téléphone valide
 				        </div>
-					</div>
 
-				  <button type="button" className="btn btn-primary mb-2" onClick={(e) => {e.preventDefault(); console.log("stop");}}>Submit</button>
+					</div>
+				    <button type="button" className="col-12 col-sm-1 align-self-start btn btn-primary adapt-to-icon" onClick={(e) => {e.preventDefault(); this.handleForm();}}>Submit</button>
 				</form>
 			</div>
 		);
